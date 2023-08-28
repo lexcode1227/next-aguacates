ee039dd37015d2f617596fc69a94201f178e470c
Henry Agustin <henryagustin297@gmail.com>
2023-08-28 16:26:06 -0600

    refactorización en el codigo de la pagina dinamica de product

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,24 +1,14 @@
 'use client'
-import {React, useState, useEffect} from 'react'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import Item from '@/components/Item'
 import { Col, Spin } from 'antd'
 
 const ProductItem = ({ params }) => {
-  const id = params.id
-  const [loading, setLoading] = useState(true)
-  const [avo, setAvo] = useState([]);
+  const id = params.id;
+  const avo = useSelector((state)=> state.cart.avocadoList.find(item => item.avocado.id === id));
+  const loading = useSelector((state) => state.cart.loading);
 
-  useEffect(() => {
-    fetch(`https://platzi-avo.vercel.app/api/avo/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setAvo(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
   return (
     loading ? 
     <Col className="text-center"> <Spin spinning size='large'/> </Col> 
